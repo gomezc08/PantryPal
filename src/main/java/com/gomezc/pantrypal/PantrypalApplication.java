100636dc2e570140ee926823fa4e8d3102d279c2
Chris G <gomezc@whitman.edu>
2025-01-26 20:33:14 -0500

    initalize project

---
+++ src/main/java/com/gomezc/pantrypal/PantrypalApplication.java
@@ -0,0 +1,13 @@
+package com.gomezc.pantrypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantrypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantrypalApplication.class, args);
+	}
+
+}
